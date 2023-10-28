c4400a299edfa10efe7cc5fb87833e5f7834b89b
Vladislav <vladisgnatenko@gmail.com>
2023-10-28 15:49:13 +0300

    Initial Commit

---
+++ WBC_Lab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WBC_Lab
+//
+//  Created by Vlad Ihnatenko on 28.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WBC_Lab/WBC_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBC_LabApp.swift
+//  WBC_Lab
+//
+//  Created by Vlad Ihnatenko on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WBC_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
